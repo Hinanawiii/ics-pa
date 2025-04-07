ed0c3979ade77359863c943cbe12cfec1cdd8792
tracer-ics2017 <tracer@njuics.org>
2025-04-08 00:08:26 +0800

    >  compile 2212338 hinanawi Linux hinanawi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  00:08:26 up 6 days,  6:41,  1 user,  load average: 0.25, 0.18, 0.14 8bc583ed3d95f9f5a27d9a581ba8c3c36edfa

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -6,8 +6,10 @@ make_EHelper(mov) {
 }
 //为了防止其他形式，这些先保留
 make_EHelper(push) {
+printf("开始执行push指令: EIP=0x%x\n", cpu.eip);
   uint8_t reg = decoding.opcode & 0x7;
   rtl_push(&reg_l(reg));
+  printf("执行完push指令: EIP=0x%x\n", cpu.eip);
   print_asm_template1(push);
 }
 

e18f832fa84c1a0fec3ed49de623490a878c3d25
tracer-ics2017 <tracer@njuics.org>
2025-04-08 05:30:12 +0800

    >  compile 2212338 hinanawi Linux hinanawi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  05:30:12 up 6 days, 12:03,  1 user,  load average: 0.45, 0.34, 0.30 e33452b7597e9f48b84743736e7d6c1a43d7158a

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -46,7 +46,7 @@ void pio_write(ioaddr_t, int, uint32_t);
 
 make_EHelper(in) {
   uint32_t port_val = pio_read(id_src->val, id_dest->width);
-  printf("Reading port 0x%x, width %d, value 0x%x\n", id_src->val, id_dest->width, port_val);
+  //printf("Reading port 0x%x, width %d, value 0x%x\n", id_src->val, id_dest->width, port_val);
   rtl_li(&t0, port_val);
   operand_write(id_dest, &t0);
   print_asm_template2(in);

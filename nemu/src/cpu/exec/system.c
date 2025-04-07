bcddcea033e300b1ee7e385a10015a9f2dadb976
tracer-ics2017 <tracer@njuics.org>
2025-04-08 05:29:26 +0800

    >  compile 2212338 hinanawi Linux hinanawi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  05:29:26 up 6 days, 12:02,  1 user,  load average: 0.35, 0.32, 0.29 b8ef9e462198c6f5c31dfe71134c021ff3c34c17

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -45,20 +45,10 @@ uint32_t pio_read(ioaddr_t, int);
 void pio_write(ioaddr_t, int, uint32_t);
 
 make_EHelper(in) {
-  uint32_t port_val;
-  
-  // 特殊情况：对串口状态寄存器(0x3FD)的读取，始终返回0x20
-  if (id_src->val == 0x3FD) {
-    port_val = 0x20;  // 串口总是就绪的
-  } else {
-    port_val = pio_read(id_src->val, id_dest->width);
-  }
-  
-  //printf("Reading port 0x%x, width %d, value 0x%x\n", id_src->val, id_dest->width, port_val);
-  
+  uint32_t port_val = pio_read(id_src->val, id_dest->width);
+  printf("Reading port 0x%x, width %d, value 0x%x\n", id_src->val, id_dest->width, port_val);
   rtl_li(&t0, port_val);
   operand_write(id_dest, &t0);
-  
   print_asm_template2(in);
 #ifdef DIFF_TEST
   diff_test_skip_qemu();

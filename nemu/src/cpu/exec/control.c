3d5a30900bdd8c6e007d2ae1f509af7ec2bd3080
tracer-ics2017 <tracer@njuics.org>
2025-04-08 02:33:26 +0800

    >  compile 2212338 hinanawi Linux hinanawi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  02:33:26 up 6 days,  9:06,  1 user,  load average: 0.41, 0.39, 0.25 760c757759248e6667fd796bbae86fd41403e34

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -18,6 +18,7 @@ make_EHelper(jmp_rm) {
   decoding.jmp_eip = id_dest->val;
   decoding.is_jmp = 1;
   print_asm("jmp *%s", id_dest->str);
+  printf("Indirect jump/call to address 0x%x\n", id_dest->val);
 }
 
 make_EHelper(call) {
@@ -63,4 +64,5 @@ make_EHelper(call_rm) {
   decoding.jmp_eip = id_dest->val;  
   decoding.is_jmp = 1;
   print_asm("call *%s", id_dest->str);
+  printf("Indirect jump/call to address 0x%x\n", id_dest->val);
 }

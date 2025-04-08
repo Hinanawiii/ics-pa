5411f1bc54efd7ee9d05907fdf4e7d45af678dea
tracer-ics2017 <tracer@njuics.org>
2025-04-08 11:31:41 +0800

    >  compile 2212338 hinanawi Linux hinanawi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  11:31:41 up 6 days, 18:05,  1 user,  load average: 0.41, 0.67, 0.73 a0fda901ee82d2ba7d8cca87b2d497aba3f2e23c

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -24,7 +24,7 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
   
   int mmio_id = is_mmio(addr);
   if (mmio_id != -1) {
-  	printf("%d",len);
+  	//printf("%d",len);
     mmio_write(addr, len, data, mmio_id);
   } else {
     memcpy(guest_to_host(addr), &data, len);

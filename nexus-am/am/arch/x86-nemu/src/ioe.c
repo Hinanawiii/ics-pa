25cc2b0252c96f642c0eb09a5e719748e3320389
tracer-ics2017 <tracer@njuics.org>
2025-04-08 10:34:39 +0800

    >  run 2212338 hinanawi Linux hinanawi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  10:34:39 up 6 days, 17:07,  1 user,  load average: 0.32, 0.27, 0.36 a623f05f7c71638447c381d1383a522758b12b6d

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -5,6 +5,10 @@
 #define I8042_STATUS_PORT 0x64
 #define I8042_STATUS_HASKEY_MASK 0x1
 #define KEYBOARD_IRQ 1
+#define VMEM 0x40000
+
+#define SCREEN_H 300
+#define SCREEN_W 400
 static unsigned long boot_time;
 
 void _ioe_init() {
@@ -33,12 +37,29 @@ _Screen _screen = {
 extern void* memcpy(void *, const void *, int);
 
 void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
-  int i;
-  for (i = 0; i < _screen.width * _screen.height; i++) {
-    fb[i] = i;
+  int i, j;
+  uint32_t *fb = (uint32_t *)(uintptr_t)VMEM;
+  
+  // 遍历需要绘制的矩形区域
+  for (i = 0; i < h; i++) {
+    for (j = 0; j < w; j++) {
+      // 计算目标位置
+      int dest_x = x + j;
+      int dest_y = y + i;
+      
+      // 检查坐标是否在屏幕范围内
+      if (dest_x >= 0 && dest_x < _screen.width && 
+          dest_y >= 0 && dest_y < _screen.height) {
+        // 计算源和目标的索引
+        int src_idx = i * w + j;
+        int dest_idx = dest_y * _screen.width + dest_x;
+        
+        // 将像素数据复制到帧缓冲区
+        fb[dest_idx] = pixels[src_idx];
+      }
+    }
   }
 }
-
 void _draw_sync() {
 }
 

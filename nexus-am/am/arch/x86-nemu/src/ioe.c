d5273f9753a521c3783f0bc30fe8265e5646451c
tracer-ics2017 <tracer@njuics.org>
2025-04-08 10:17:21 +0800

    >  run 2212338 hinanawi Linux hinanawi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  10:17:21 up 6 days, 16:50,  1 user,  load average: 0.47, 0.51, 0.66 1cac4d351f9602d641ca1705c00e3d5e4a71b13

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -1,6 +1,10 @@
 #include <am.h>
 #include <x86.h>
 #define RTC_PORT 0x48   // Note that this is not standard
+#define I8042_DATA_PORT 0x60
+#define I8042_STATUS_PORT 0x64
+#define I8042_STATUS_HASKEY_MASK 0x1
+#define KEYBOARD_IRQ 1
 static unsigned long boot_time;
 
 void _ioe_init() {
@@ -39,5 +43,10 @@ void _draw_sync() {
 }
 
 int _read_key() {
-  return _KEY_NONE;
+    if (inb(I8042_STATUS_PORT) & I8042_STATUS_HASKEY_MASK) {
+    // 如果有按键事件，则读取数据端口获取键码
+    return inl(I8042_DATA_PORT);
+  } else {
+    return _KEY_NONE;
+  }
 }

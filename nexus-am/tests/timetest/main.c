a861510062883c9f24ad335ef173b0ffa6193c7a
tracer-ics2017 <tracer@njuics.org>
2025-04-08 09:47:28 +0800

    >  run 2212338 hinanawi Linux hinanawi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  09:47:28 up 6 days, 16:20,  1 user,  load average: 0.06, 0.11, 0.06 7ded14a7a3f111c8eba01b5978bfdd2d61251a

--- nexus-am/tests/timetest/main.c
+++ nexus-am/tests/timetest/main.c
@@ -6,23 +6,20 @@ int main(){
   int sec = 1;
   
   // 初始输出当前时间
-  printf("Initial uptime: %lu ms\n", _uptime());
+  unsigned long initial_time = _uptime();
+  printf("Initial uptime: %d ms\n", (int)initial_time);
   
   while (1) {
     // 等待到达下一秒
-    while(_uptime() < 1000 * sec) {
-      // 每隔一段时间输出当前时间值
-      if (_uptime() % 100 == 0) {
-        printf("Current uptime: %lu ms, target: %d ms\n", _uptime(), 1000 * sec);
-      }
-    }
+    while(_uptime() < 1000 * sec);
     
     // 达到目标时间时输出
+    unsigned long current_time = _uptime();
     printf("Reached %d ", sec);
     if (sec == 1) {
-      printf("second. (uptime: %lu ms)\n", _uptime());
+      printf("second. (uptime: %d ms)\n", (int)current_time);
     } else {
-      printf("seconds. (uptime: %lu ms)\n", _uptime());
+      printf("seconds. (uptime: %d ms)\n", (int)current_time);
     }
     sec++;
   }

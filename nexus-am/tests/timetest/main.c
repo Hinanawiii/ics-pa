4cfc2b43f1705c83983ed655968cd4feb74b2b73
tracer-ics2017 <tracer@njuics.org>
2025-04-08 09:41:48 +0800

    >  run 2212338 hinanawi Linux hinanawi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  09:41:48 up 6 days, 16:15,  1 user,  load average: 0.23, 0.07, 0.02 1a5c2c28182eab58fb63628759ac4a632975d4e

--- nexus-am/tests/timetest/main.c
+++ nexus-am/tests/timetest/main.c
@@ -4,14 +4,25 @@
 int main(){
   _ioe_init();
   int sec = 1;
+  unsigned long last_time = 0;
+  
   while (1) {
+    unsigned long current_time = _uptime();
+    
+    // 打印时间差，看是否正常递增
+    if (current_time > last_time) {
+      _putc('.');  // 每当时间变化就输出一个点
+      last_time = current_time;
+    }
+    
     while(_uptime() < 1000 * sec) ;
+    
     if (sec == 1) {
       printf("%d second.\n", sec);
     } else {
       printf("%d seconds.\n", sec);
     }
-    sec ++;
+    sec++;
   }
   return 0;
 }

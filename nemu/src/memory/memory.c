62b8e61dc8dc366c5c27f91934a907f019fb97bb
tracer-ics2017 <tracer@njuics.org>
2025-04-08 10:27:25 +0800

    >  compile 2212338 hinanawi Linux hinanawi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  10:27:25 up 6 days, 17:00,  1 user,  load average: 0.08, 0.19, 0.44 d5c1201c3ab2068de3969f15641a7f2dd7af60b1

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,5 +1,5 @@
 #include "nemu.h"
-
+#include "device/mmio.h"
 #define PMEM_SIZE (128 * 1024 * 1024)
 
 #define pmem_rw(addr, type) *(type *)({\
@@ -10,12 +10,20 @@
 uint8_t pmem[PMEM_SIZE];
 
 /* Memory accessing interfaces */
-
 uint32_t paddr_read(paddr_t addr, int len) {
+  int mmio_id = is_mmio(addr);
+  if (mmio_id != -1) {
+    return mmio_read(mmio_id, addr, len);
+  }
   return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
 }
 
 void paddr_write(paddr_t addr, int len, uint32_t data) {
+  int mmio_id = is_mmio(addr);
+  if (mmio_id != -1) {
+    mmio_write(mmio_id, addr, len, data);
+    return;
+  }
   memcpy(guest_to_host(addr), &data, len);
 }
 

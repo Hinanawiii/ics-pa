dead671951d315bcda35655c35cb94768121607d
tracer-ics2017 <tracer@njuics.org>
2025-04-29 00:38:17 +0800

    >  compile 2212338 hinanawi Linux hinanawi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  00:38:17 up  3:43,  1 user,  load average: 0.18, 0.15, 0.10 638fef62678ac1ed5de790cb27961cfc7956c5b3

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -46,16 +46,24 @@ make_EHelper(int) {
 }
 
 make_EHelper(iret) {
-  rtl_pop(&cpu.eip);
+  // 打印当前栈信息进行调试
+  rtlreg_t ret_addr;
+  rtl_lr(&ret_addr, R_ESP, 0);
+  printf("iret: stack top contains 0x%x\n", ret_addr);
   
-  // 使用临时变量解决类型不匹配问题
+  // 标准iret实现
+  rtl_pop(&cpu.eip);
+  printf("iret: setting EIP to 0x%x\n", cpu.eip);
+  
   rtlreg_t cs_temp;
   rtl_pop(&cs_temp);
   cpu.cs = (uint16_t)cs_temp;
   
   rtl_pop(&t0);
   memcpy(&cpu.eflags, &t0, sizeof(cpu.eflags));
-  decoding.jmp_eip = true;
+  
+  // 使用你原来的设置方式
+  decoding.jmp_eip = 1;
   decoding.seq_eip = cpu.eip;
   
   print_asm("iret");

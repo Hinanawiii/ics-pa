cbfe58918bebd30e8cd072e1015ca1018d1d848c
tracer-ics2017 <tracer@njuics.org>
2025-04-28 21:36:35 +0800

    >  run 2212338 hinanawi Linux hinanawi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  21:36:35 up 41 min,  1 user,  load average: 0.41, 0.31, 0.20 69a9a582f6788a652656a22cdc1e8cca9e6edfc9

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -16,18 +16,22 @@ _RegSet* do_syscall(_RegSet *r) {
       Log("Program exit with code %d", a[1]);
       _halt(a[1]);  // 使用参数作为退出状态
       break;
-    case SYS_write:
-      // a[1]是fd，a[2]是buf的地址，a[3]是长度
-      if (a[1] == 1 || a[1] == 2) {  // stdout或stderr
-        // 逐个字符输出到串口
-        for (int i = 0; i < a[3]; i++) {
-          _putc(*(char *)(a[2] + i));
-        }
-        SYSCALL_ARG1(r) = a[3];  // 返回写入的字节数
-      } else {
-        SYSCALL_ARG1(r) = -1;  // 错误
-      }
-      break;
+		case SYS_write: {
+			Log("Handling SYS_write: fd=%d, buf=%p, count=%d", a[1], (void*)a[2], a[3]);
+			if (a[1] == 1 || a[1] == 2) {
+				// 输出到串口
+				for (int i = 0; i < a[3]; i++) {
+				  char c = *(char*)(a[2] + i);
+				  _putc(c);
+				  Log("Output character: %c", c);
+				}
+				SYSCALL_ARG1(r) = a[3];
+			} else {
+				// ...
+			}
+			Log("SYS_write completed, returned %d", SYSCALL_ARG1(r));
+			break;
+		}
   	case SYS_brk:
   	// 在目前的Nanos-lite中，我们总是返回0表示成功
   		SYSCALL_ARG1(r) = 0;

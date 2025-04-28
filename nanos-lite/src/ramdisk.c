06acbf81bbeee58265c97e7caedb42b198505c64
tracer-ics2017 <tracer@njuics.org>
2025-04-28 22:45:54 +0800

    >  run 2212338 hinanawi Linux hinanawi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  22:45:54 up  1:51,  1 user,  load average: 0.07, 0.12, 0.14 369a066e90f61bbeec60ca28254995e8e80736a

--- nanos-lite/src/ramdisk.c
+++ nanos-lite/src/ramdisk.c
@@ -2,7 +2,7 @@
 
 extern uint8_t ramdisk_start;
 extern uint8_t ramdisk_end;
-#define RAMDISK_SIZE ramdisk_end-ramdisk_start
+#define RAMDISK_SIZE ((uintptr_t)(&ramdisk_end) - (uintptr_t)(&ramdisk_start))
 
 /* The kernel is monolithic, therefore we do not need to
  * translate the address `buf' from the user process to

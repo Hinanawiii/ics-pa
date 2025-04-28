cb09aaaf3ba17f6dc07eedf0ec900209e958736d
tracer-ics2017 <tracer@njuics.org>
2025-04-29 00:26:44 +0800

    >  compile 2212338 hinanawi Linux hinanawi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  00:26:44 up  3:32,  1 user,  load average: 0.10, 0.12, 0.07 442c5f883494cf140f9ad2af39a70201323f649

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -4,13 +4,19 @@ void diff_test_skip_qemu();
 void diff_test_skip_nemu();
 
 make_EHelper(lidt) {
-  cpu.idtr.limit = vaddr_read(id_dest->addr, 2);
-  if (decoding.is_operand_size_16) {
-    cpu.idtr.base = vaddr_read(id_dest->addr + 2, 3);
-  } else {
-    cpu.idtr.base = vaddr_read(id_dest->addr + 2, 4);
-  }
+  // TODO();
+  t1 = id_dest -> val;
+  rtl_lm(&t0, &t1, 2);
+  cpu.idtr.limit = t0;
 
+  t1 = id_dest -> val + 2;
+  rtl_lm(&t0, &t1, 4);
+  cpu.idtr.base = t0;
+
+#ifdef DEBUG
+  Log("idtr.limit=0x%x", cpu.idtr.limit);
+  Log("idtr.base=0x%x", cpu.idtr.base);
+#endif
   print_asm_template1(lidt);
 }
 

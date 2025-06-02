c629c3affdc61dd7d2e6b939f90d1a485e8393a3
tracer-ics2017 <tracer@njuics.org>
2025-06-03 00:01:36 +0800

    >  run 2212338 hinanawi Linux hinanawi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  00:01:36 up 3 days, 21:22,  1 user,  load average: 0.36, 0.42, 0.24 21cede3b97e511362d8fcecb8e8822217097907

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -12,6 +12,7 @@ static inline _RegSet* sys_exit(_RegSet *r){
 }
 
 static inline _RegSet* sys_write(_RegSet *r){
+/*
    int fd = (int)SYSCALL_ARG2(r);
    char *buf = (char *)SYSCALL_ARG3(r);
    int len = (int)SYSCALL_ARG4(r);
@@ -23,14 +24,14 @@ static inline _RegSet* sys_write(_RegSet *r){
      SYSCALL_ARG1(r) = len;
    }
 	 //Log("used sys_write!");
-   return NULL;
-	/*
+   return NULL;*/
+	
   int fd = (int)SYSCALL_ARG2(r);
   char *buf = (char *)SYSCALL_ARG3(r);
   int len = (int)SYSCALL_ARG4(r);
   SYSCALL_ARG1(r) = fs_write(fd,buf,len);
 
-  return NULL;*/
+  return NULL;
 }
 
 

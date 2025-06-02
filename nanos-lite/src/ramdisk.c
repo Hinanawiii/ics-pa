6a9acbb6bf1ff400bbffc433f4c2cab098415a2e
tracer-ics2017 <tracer@njuics.org>
2025-06-02 21:57:30 +0800

    >  run 2212338 hinanawi Linux hinanawi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  21:57:30 up 3 days, 19:18,  1 user,  load average: 0.28, 0.16, 0.11 8b8ff04461b77735b7fb8690723fca946c31e517

--- nanos-lite/src/ramdisk.c
+++ nanos-lite/src/ramdisk.c
@@ -11,14 +11,14 @@ extern uint8_t ramdisk_end;
 
 /* read `len' bytes starting from `offset' of ramdisk into `buf' */
 void ramdisk_read(void *buf, off_t offset, size_t len) {
-	Log("READ offset:&s,len:&s,RAMDISK_SIZE:&s",offset,len,RAMDISK_SIZE);
+	Log("READ offset:%p,len:%d,RAMDISK_SIZE:%d",offset,len,RAMDISK_SIZE);
   assert(offset + len <= RAMDISK_SIZE);
   memcpy(buf, &ramdisk_start + offset, len);
 }
 
 /* write `len' bytes starting from `buf' into the `offset' of ramdisk */
 void ramdisk_write(const void *buf, off_t offset, size_t len) {
-	Log("WRITE offset:&s,len:&s,RAMDISK_SIZE:&s",offset,len,RAMDISK_SIZE);
+	Log("WRITE offset:%p,len:%d,RAMDISK_SIZE:%d",offset,len,RAMDISK_SIZE);
   assert(offset + len <= RAMDISK_SIZE);
   memcpy(&ramdisk_start + offset, buf, len);
 }

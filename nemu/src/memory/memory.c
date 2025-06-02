f77577fcf75bf1ed2611f94ba5ddf94a000ff77c
tracer-ics2017 <tracer@njuics.org>
2025-06-02 19:48:31 +0800

    >  compile 2212338 hinanawi Linux hinanawi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:48:31 up 3 days, 17:09,  1 user,  load average: 0.61, 0.40, 0.36 ea750a8a32f3e4bc748383f0cc1851899fac4519

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,6 +1,5 @@
 #include "nemu.h"
 #include "device/mmio.h"
-
 #define PMEM_SIZE (128 * 1024 * 1024)
 
 #define pmem_rw(addr, type) *(type *)({\
@@ -11,28 +10,26 @@
 uint8_t pmem[PMEM_SIZE];
 
 /* Memory accessing interfaces */
-
 uint32_t paddr_read(paddr_t addr, int len) {
-  int map_NO = is_mmio(addr);
-  if (map_NO == -1)
-    return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
-  else
-    return mmio_read(addr, len, map_NO);
+  int mmio_id = is_mmio(addr);
+  if (mmio_id != -1) {
+    return mmio_read(mmio_id, addr, len);
+  }
+  return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
 }
 
 void paddr_write(paddr_t addr, int len, uint32_t data) {
-  int map_NO = is_mmio(addr);
-  if (map_NO == -1) {
-    uint8_t *p = (uint8_t *)guest_to_host(addr);
-    while (len--) {
-      *p++ = data & 0xff;
-      data >>= 8;
-    }
+  // 确保长度是有效的
+  assert(len >= 1 && len <= 4);
+  
+  int mmio_id = is_mmio(addr);
+  if (mmio_id != -1) {
+  	//printf("%d",len);
+    mmio_write(addr, len, data, mmio_id);
   } else {
-    mmio_write(addr, len, data, map_NO);
+    memcpy(guest_to_host(addr), &data, len);
   }
 }
-
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
   return paddr_read(addr, len);

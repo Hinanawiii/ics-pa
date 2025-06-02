6267406308b3723b2adf8d911b4ebf6e332b62fe
tracer-ics2017 <tracer@njuics.org>
2025-06-02 22:01:54 +0800

    >  run 2212338 hinanawi Linux hinanawi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  22:01:54 up 3 days, 19:23,  1 user,  load average: 0.06, 0.11, 0.09 41457a6fafd611ca8ce98c5f29f3e92db2706e2c

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -42,6 +42,7 @@ int fs_open(const char *pathname, int flags, int mode) {
 	Log("Path: %s", pathname);
 	int i;
 	for (i = 0; i < NR_FILES; i++) {
+	  Log("filename:%s\n",file_table[i].name);
 		if (strcmp(file_table[i].name, pathname) == 0) {
 			return i;
 		}

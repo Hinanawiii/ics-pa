bd5aa925599689ea8a05f878c9544a83a41d84b5
tracer-ics2017 <tracer@njuics.org>
2025-06-02 20:20:43 +0800

    >  run 2212338 hinanawi Linux hinanawi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  20:20:43 up 3 days, 17:41,  1 user,  load average: 0.61, 0.39, 0.31 f81ab65f7e658f5aed0b76477e760d3ffa9338a

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -12,27 +12,27 @@ static inline _RegSet* sys_exit(_RegSet *r){
 }
 
 static inline _RegSet* sys_write(_RegSet *r){
-  // //获取三个参数：fd buf len
-  // int fd = (int)SYSCALL_ARG2(r);
-  // char *buf = (char *)SYSCALL_ARG3(r);
-  // int len = (int)SYSCALL_ARG4(r);
-  // // Log("!");
-  // if(fd == 1 || fd == 2){
-  //   for(int i = 0; i < len; i++) {
-  //       _putc(buf[i]);
-  //   }
-  //   //根据man 返回len
-  //   SYSCALL_ARG1(r) = len;
-  // }
+   //获取三个参数：fd buf len
+   int fd = (int)SYSCALL_ARG2(r);
+   char *buf = (char *)SYSCALL_ARG3(r);
+   int len = (int)SYSCALL_ARG4(r);
+   // Log("!");
+   if(fd == 1 || fd == 2){
+     for(int i = 0; i < len; i++) {
+         _putc(buf[i]);
+     }
+     //根据man 返回len
+     SYSCALL_ARG1(r) = len;
+   }
 
-  // return NULL;
-
+   return NULL;
+	/*
   int fd = (int)SYSCALL_ARG2(r);
   char *buf = (char *)SYSCALL_ARG3(r);
   int len = (int)SYSCALL_ARG4(r);
   SYSCALL_ARG1(r) = fs_write(fd,buf,len);
   //Log("used sys_write!");
-  return NULL;
+  return NULL;*/
 }
 
 

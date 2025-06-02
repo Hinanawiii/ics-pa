c6a36cd60770b398a3aac9718bf538c0fcb6419f
tracer-ics2017 <tracer@njuics.org>
2025-06-02 19:40:02 +0800

    >  compile 2212338 hinanawi Linux hinanawi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:40:02 up 3 days, 17:01,  1 user,  load average: 0.42, 0.26, 0.29 d65c3112f467e0181744be7d0bfaac814fb2117

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -1,69 +1,17 @@
 #include "monitor/watchpoint.h"
 #include "monitor/expr.h"
-#include "nemu.h"
 
 #define NR_WP 32
 
 static WP wp_pool[NR_WP];
-WP *head = NULL;
-WP *free_ = NULL;
+static WP *head, *free_;
+int used_next;
 
-
-WP* new_wp(){
-	
-	if(free_ == NULL){
-		panic("NO FREE Watchpoints! ");
-	}	
-	
-  WP *wp = free_;
-  free_ = free_->next;
-  wp->next = head;
-  head = wp;
-    
-  return wp;
-}
-
-void free_wp(WP* wp)
-{
-    // 从head链表移除
-    if (head == wp) {
-        head = head->next;
-    }
-    else
-    {
-    	WP *prev = head;
-      while (prev->next != wp) {
-          prev = prev->next;
-          if (prev == NULL) return; 
-      }
-      prev->next = wp->next;
-    }
-    
-    wp->next = free_;
-    free_ = wp;
-}
-//管理监视点
-void check_watchpoints() {
-    WP *wp = head;
-    while (wp != NULL) {
-        bool success;
-        uint32_t new_val = expr(wp->expr, &success);
-        
-        if (success && new_val != wp->old_val) {
-            nemu_state = NEMU_STOP;
-            printf("Watchpoint %d: %s\n", wp->NO, wp->expr);
-            printf("Old value = %u\n", wp->old_val);
-            printf("New value = %u\n", new_val);
-            wp->old_val = new_val;
-        }
-        wp = wp->next;
-    }
-}
-//检查
 void init_wp_pool() {
   int i;
   for (i = 0; i < NR_WP; i ++) {
     wp_pool[i].NO = i;
+    wp_pool[i].result = 0;
     wp_pool[i].next = &wp_pool[i + 1];
   }
   wp_pool[NR_WP - 1].next = NULL;
@@ -73,5 +21,79 @@ void init_wp_pool() {
 }
 
 /* TODO: Implement the functionality of watchpoint */
+
+WP* new_wp()
+{
+    if(free_ == NULL) {
+        assert(0);
+    }
+
+    WP *temp = free_;
+    free_ = free_->next;
+
+    if(head == NULL) {
+        head = temp;
+    } else {    
+        WP *search_head = head;
+        while(search_head->next != NULL) {
+            search_head = search_head->next;
+        }
+        search_head->next = temp;
+    }
+
+    temp->next = NULL;
+    return temp;
+}
+void free_wp(int wpid)
+{
+    WP* wp = &wp_pool[wpid];
+    WP *temp = head;
+
+    if(temp == wp) {
+        head = wp->next;
+    } else {    
+        while(temp->next != wp) {
+            temp = temp->next;
+        }
+        temp->next = wp->next;
+    }
+
+    wp->next = free_;
+    free_ = wp;
+}
+
+bool check_watchpoint()
+{
+	WP *wp=head;
+	bool* success_flag=malloc(1);
+	bool ret=true;	
+	while(wp!=NULL)
+	{
+		uint32_t new_value=expr(wp->expr,success_flag);
+		//printf("new value %d\n",new_value);
+		if(new_value!=wp->expr_record_val){
+			printf("\nHit watchpoint %d  old value = 0x%x, new value = 0x%x .\n"
+				,wp->NO,wp->expr_record_val,new_value);
+			ret=false;
+			goto end;			
+		}
+		wp=wp->next;
+	}
+end:	
+	//free(success_flag);
+	return ret;
+}
+
+void info_watchpoint()
+{
+	WP *temp=head;	
+	printf("WID	expr		record value\n");
+	while(temp!=NULL)
+	{
+		printf("%d	%s		0x%x\n",temp->NO,temp->expr,temp->expr_record_val);
+		temp=temp->next;
+	}
+}
+
 
 

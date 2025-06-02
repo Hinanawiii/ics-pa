3e2158a705ede418b11daa7f91c463630a9676a1
tracer-ics2017 <tracer@njuics.org>
2025-06-02 17:08:48 +0800

    >  compile 2212338 hinanawi Linux hinanawi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  17:08:48 up 3 days, 14:30,  1 user,  load average: 0.49, 0.19, 0.07 336d4865c2e324b79da44819be5f6a0c3513c35

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -2,11 +2,38 @@
 #include "memory/mmu.h"
 
 void raise_intr(uint8_t NO, vaddr_t ret_addr) {
-  /* TODO: Trigger an interrupt/exception with ``NO''.
-   * That is, use ``NO'' to index the IDT.
-   */
+    // 计算中断门描述符地址
+    vaddr_t gate_addr = cpu.idtr.base + NO * 8;
+   	if(cpu.idtr.limit <0){
+   		assert(0);
+   	} //检查整个
+    // 读取中断门描述符（64 位）
+    uint32_t low = vaddr_read(gate_addr, 4)&0xffff ;     
+    uint32_t high = vaddr_read(gate_addr + 4, 4)& 0xffff0000;
 
-  TODO();
+    //if (!((high >> 8) & 1)) {
+    //    panic("Interrupt gate not present! NO = %d", NO);
+    //}
+
+    //uint32_t offset = (high & 0xFFFF0000) | (low & 0x0000FFFF);
+
+    // 保存上下文
+    cpu.esp -= 4;
+    vaddr_write(cpu.esp, 4,  cpu.eflags.val);  
+    cpu.esp -= 4;
+    vaddr_write(cpu.esp, 4,  cpu.cs);       
+    cpu.esp -= 4;
+    vaddr_write(cpu.esp, 4,  ret_addr);    
+           
+
+		decoding.jmp_eip = high|low;
+		decoding.is_jmp = true;  
+
+    // 如果是中断门（type=0xE），清除 IF 位
+    uint8_t type = (high >> 8) & 0xFF ;
+    if (type == 0xE) {
+        cpu.eflags.IF = 0;  
+    }
 }
 
 void dev_raise_intr() {

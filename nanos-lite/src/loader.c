61e8c6299a8032e76e9100e94681aa99d9570d17
tracer-ics2017 <tracer@njuics.org>
2025-06-02 23:02:44 +0800

    >  run 2212338 hinanawi Linux hinanawi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  23:02:44 up 3 days, 20:24,  1 user,  load average: 0.10, 0.51, 0.41 9f2526e03d36a78b2dbe66d5864b364e8ec8884

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -15,13 +15,13 @@ size_t get_ramdisk_size();
 uintptr_t loader(_Protect *as, const char *filename) {
   // TODO();
 
-  //ramdisk_read(DEFAULT_ENTRY,0,get_ramdisk_size()); 
-  int fd = fs_open(filename, 0, 0);
-  Log("fd=%d\n",fd);
-  size_t f_size = fs_filesz(fd);
-  Log("filesize=%d",f_size);
-  fs_read(fd, DEFAULT_ENTRY, f_size);
-  fs_close(fd);
+  ramdisk_read(DEFAULT_ENTRY,0,get_ramdisk_size()); 
+  //int fd = fs_open(filename, 0, 0);
+  //Log("fd=%d\n",fd);
+  //size_t f_size = fs_filesz(fd);
+  //Log("filesize=%d",f_size);
+  //fs_read(fd, DEFAULT_ENTRY, f_size);
+  //fs_close(fd);
   
   return (uintptr_t)DEFAULT_ENTRY;
 

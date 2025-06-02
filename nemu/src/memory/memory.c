9fe9194668c3b5df34417f15c0f8c50b245ee63e
tracer-ics2017 <tracer@njuics.org>
2025-06-03 03:57:49 +0800

    >  compile 2212338 hinanawi Linux hinanawi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  03:57:49 up 4 days,  1:19,  1 user,  load average: 0.15, 0.18, 0.18 278d1eee2503ac35ae031041df23ac6307dcc63

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -35,69 +35,65 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
 }
 
 paddr_t page_translate(vaddr_t addr, int rw) {
-    // 检查保护模式和分页是否启用
     if (!cpu.cr0.protect_enable || !cpu.cr0.paging)
         return addr;
 
-    PDE* pd = (PDE*)(cpu.cr3.val & 0xfffff000);
-    int pd_index = (addr >> 22) & 0x3ff;  // 页目录索引
-    PTE* pt;
+  PDE* pd = (PDE*)(cpu.cr3.val & 0xfffff000);
+  int pd_index = (addr >> 22) & 0x3ff;
+  PTE* pt;
+  
+  PDE pde;
+  pde.val = paddr_read((paddr_t)&pd[pd_index], 4);
+  if (!pde.present)
+    Assert(0, "pde present bit is 0!");
 
-    PDE pde;
-    pde.val = paddr_read((paddr_t)&pd[pd_index], 4);
-    if (!pde.present)
-        Assert(0, "pde present bit is 0!");
-    pt = (PTE*)(pde.val & ~0xfff);
-    
+  pt = (PTE*)(pde.val & ~0xfff);
+  pde.accessed = 1;
+  paddr_write((paddr_t)&pd[pd_index], 4, pde.val);
 
-    pde.accessed = 1;
-    paddr_write((paddr_t)&pd[pd_index], 4, pde.val);
+  int pt_index = (addr >> 12) & 0x3ff;
+  PTE pte;
+  pte.val = paddr_read((paddr_t)&pt[pt_index], 4);
+  if (!pte.present)
+    Assert(0, "pte present bit is 0!");
 
-    int pt_index = (addr >> 12) & 0x3ff;
+  addr = (pte.val & ~0xfff) | (addr & 0xfff);
+  pte.accessed = 1;
+  if (rw)
+    pte.dirty = 1;
+  paddr_write((paddr_t)&pt[pt_index], 4, pte.val);
 
-    PTE pte;
-    pte.val = paddr_read((paddr_t)&pt[pt_index], 4);
-
-    if (!pte.present)
-        Assert(0, "pte present bit is 0!");
-
-    addr = (pte.val & ~0xfff) | (addr & 0xfff);
-
-    pte.accessed = 1;
-    if (rw)
-        pte.dirty = 1;
-
-    paddr_write((paddr_t)&pt[pt_index], 4, pte.val);
-
-    return (paddr_t)addr;
+  return (paddr_t)addr;
 }
 
+
+
 uint32_t vaddr_read(vaddr_t addr, int len) {
-    if (len == 0) return 0; 
-    if ((((addr) + (len) - 1) & ~PAGE_MASK) != ((addr) & ~PAGE_MASK)) {
-        // 跨页访问处理
-        uint32_t data = 0;
-        for (int i = 0; i < len; i++) {
-            paddr_t paddr = page_translate(addr + i, 0);
-            data += paddr_read(paddr, 1) << (i * 8);
-        }
-        return data;
-    } else {
-        paddr_t paddr = page_translate(addr, 0);
-        return paddr_read(paddr, len);
+  if ((((addr) + (len) - 1) & ~PAGE_MASK) != ((addr) & ~PAGE_MASK)) {
+    // 数据跨页边界
+    int i;
+    uint32_t data = 0;
+    for (i = 0; i < len; i++) {
+      paddr_t paddr = page_translate(addr + i, 0);
+      data += paddr_read(paddr, 1) << (i * 8);
     }
+    return data;
+  } else {
+    paddr_t paddr = page_translate(addr, 0);
+    return paddr_read(paddr, len);
+  }
 }
 
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
-    if (len == 0) return; 
-    if ((((addr) + (len) - 1) & ~PAGE_MASK) != ((addr) & ~PAGE_MASK)) {
-
-        for (int i = 0; i < len; i++) {
-            paddr_t paddr = page_translate(addr + i, 1);
-            paddr_write(paddr, 1, data >> (i * 8));
-        }
-    } else {
-        paddr_t paddr = page_translate(addr, 1);
-        paddr_write(paddr, len, data);
+  if ((((addr) + (len) - 1) & ~PAGE_MASK) != ((addr) & ~PAGE_MASK)) {
+    // 数据跨页边界
+    int i;
+    for (i = 0; i < len; i++) {
+      paddr_t paddr = page_translate(addr + i, 1);
+      paddr_write(paddr, 1, data >> (i * 8));
     }
+  } else {
+    paddr_t paddr = page_translate(addr, 1);
+    paddr_write(paddr, len, data);
+  }
 }

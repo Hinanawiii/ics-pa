f0cf42ba561108857fe83c6ee726e43735f1c123
tracer-ics2017 <tracer@njuics.org>
2025-06-03 04:56:05 +0800

    >  run 2212338 hinanawi Linux hinanawi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  04:56:05 up 4 days,  2:17,  1 user,  load average: 0.09, 0.18, 0.12 3f56b6ae82cf7218d19f072f65a9b53241440db

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -30,7 +30,7 @@ int main() {
 #endif
 
   init_fs();
-	 load_prog("/bin/dummy");
+	 load_prog("/bin/pal");
   //uint32_t entry = loader(NULL, "/bin/pal");
   //Log("%x",entry);
   //((void (*)(void))entry)();
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -52,9 +52,9 @@ static inline _RegSet* sys_lseek(_RegSet *r) {
   SYSCALL_ARG1(r) = fs_lseek(fd,offset,whence);
   return NULL;
 }
-
+extern int mm_brk(uint32_t new_brk);
 static inline _RegSet* sys_brk(_RegSet *r){
-  SYSCALL_ARG1(r) = 0;//总是返回0
+  SYSCALL_ARG1(r) = mm_brk(SYSCALL_ARG2(r));
   //r->eax=0;
   return NULL;
 }

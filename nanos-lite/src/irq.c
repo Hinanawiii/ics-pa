c9c7e13da6d3f53100c8b119dedd32fa3c1fb8c0
tracer-ics2017 <tracer@njuics.org>
2025-06-04 05:06:23 +0800

    >  compile 2212338 hinanawi Linux hinanawi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  05:06:23 up 4 days,  4:46,  1 user,  load average: 0.62, 0.31, 0.15 c6cc0d5ded06b68d90e67820434e266605cc6f4

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -13,6 +13,8 @@ static _RegSet* do_event(_Event e, _RegSet* r) {
     case _EVENT_TRAP:
       Log("self-trapped event!\n");
       return schedule(r);
+    case _EVENT_IRQ_TIME:
+    	return schedule(r);
     default: panic("Unhandled event ID = %d", e.event);
   }
 

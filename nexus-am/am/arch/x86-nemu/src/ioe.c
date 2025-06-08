67e728fd0dca0953edb0a2817e860ff2845d83f9
tracer-ics2017 <tracer@njuics.org>
2025-06-09 02:43:55 +0800

    >  run 2212338 hinanawi Linux hinanawi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  02:43:55 up  8:16,  1 user,  load average: 1.02, 0.49, 0.27 2ed47c1aed077d9821f05ab55dc8ca7124feb7fd

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -1,14 +1,7 @@
 #include <am.h>
 #include <x86.h>
-#define RTC_PORT 0x48   // Note that this is not standard
-#define I8042_DATA_PORT 0x60
-#define I8042_STATUS_PORT 0x64
-#define I8042_STATUS_HASKEY_MASK 0x1
-#define KEYBOARD_IRQ 1
-#define VMEM 0x40000
 
-#define SCREEN_H 300
-#define SCREEN_W 400
+#define RTC_PORT 0x48   // Note that this is not standard
 static unsigned long boot_time;
 
 void _ioe_init() {
@@ -16,15 +9,7 @@ void _ioe_init() {
 }
 
 unsigned long _uptime() {
-  static unsigned long start_time = 0;
-  unsigned long current_time = inl(RTC_PORT);
-  
-  if (start_time == 0) {
-    start_time = current_time;
-    return 0;
-  }
-  
-  return current_time - start_time;
+  return inl(RTC_PORT)-boot_time;
 }
 
 uint32_t* const fb = (uint32_t *)0x40000;
@@ -37,22 +22,22 @@ _Screen _screen = {
 extern void* memcpy(void *, const void *, int);
 
 void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
-    uint32_t *dest = fb + y * _screen.width + x;
-    for (int i = 0; i < h; i++) {
-        memcpy(dest, pixels + i * w, w * sizeof(uint32_t));
-        dest += _screen.width;
-    }
+  int i;
+  int j;
+  for (i =y ; i<y+h ; i++) {
+		  for(j=x;j<x+w;j++){
+		  	fb[j+i*_screen.width]=pixels[(j-x)+(i-y)*w];
+		  }
+  }
 }
 
 void _draw_sync() {
-	 //update_screen();
 }
 
 int _read_key() {
-    if (inb(I8042_STATUS_PORT) & I8042_STATUS_HASKEY_MASK) {
-    // 如果有按键事件，则读取数据端口获取键码
-    return inl(I8042_DATA_PORT);
-  } else {
-    return _KEY_NONE;
-  }
+   uint32_t KEY_CODE=_KEY_NONE;	
+   if(inb(0x64)&0x1){
+     KEY_CODE=inl(0x60);
+   }
+  return KEY_CODE;
 }

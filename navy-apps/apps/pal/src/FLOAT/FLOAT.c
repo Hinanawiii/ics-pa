ba82d06126b259182fb358a36842e6c70bd0f8cf
tracer-ics2017 <tracer@njuics.org>
2025-06-09 03:07:00 +0800

    >  run 2212338 hinanawi Linux hinanawi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  03:07:00 up  8:39,  1 user,  load average: 0.73, 0.91, 0.67 fdd6f697aaf72a40b975d1dcf4eefcae301480a5

--- navy-apps/apps/pal/src/FLOAT/FLOAT.c
+++ navy-apps/apps/pal/src/FLOAT/FLOAT.c
@@ -1,54 +1,33 @@
 #include "FLOAT.h"
 #include <stdint.h>
 #include <assert.h>
-#include <string.h>
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-    assert(-((int64_t)1 << 32) < ((int64_t) a * (int64_t) b) >> 16 &&
-                   ((int64_t) a * (int64_t) b) >> 16 < ((int64_t)1 << 32));
-    return ((int64_t) a * (int64_t) b) >> 16;
+	return ((int64_t)a * (int64_t)b) >> 16;
 }
 
-// FLOAT F_div_F(FLOAT a, FLOAT b) {
-//   FLOAT result = Fabs(a) / Fabs(b);
-//   FLOAT m = Fabs(a);
-//   FLOAT n = Fabs(b);
-//   m = m % n;
-
-//   for (int i = 0; i < 16; i++) {
-//     m <<= 1;
-//     result <<= 1;
-//     if (m >= n) {
-//       m -= n;
-//       result++;
-//     }
-//   }
-//   if (((a ^ b) & 0x80000000) == 0x80000000) {
-//     result = -result;
-//   }
-//   return result;
-// }
-
 FLOAT F_div_F(FLOAT a, FLOAT b) {
-    int op = 1;
-    if(a < 0) {
-        op = -op;
-        a = -a;
-    }
-    if(b < 0) {
-        op = -op;
-        b = -b;
-    }
-    int ret = a / b;
-    a %= b;
-    int i;
-    for (i = 0;i < 16;i ++){
-        a <<= 1;
-        ret <<= 1;
-        if (a >= b) a -= b, ret |= 1;
-    }
-    return op * ret;
+	if(b!=0){
+		FLOAT x = Fabs(a);
+  	FLOAT y = Fabs(b);
+  	FLOAT ret = x / y;
+  	x = x % y;
 
+  	for (int i = 0; i < 16; i++) {
+    	x <<= 1;
+    	ret <<= 1;
+    	if (x >= y) {
+      	x -= y;
+      	ret++;
+    	}
+  	}
+  	if (((a ^ b) & 0x80000000) == 0x80000000) {
+    	ret = -ret;
+  	}
+  	return ret;
+	}else{
+		assert(0);
+	}
 }
 
 FLOAT f2F(float a) {
@@ -61,42 +40,49 @@ FLOAT f2F(float a) {
    * stack. How do you retrieve it to another variable without
    * performing arithmetic operations on it directly?
    */
-  union float_ {
-    struct {
-      uint32_t m : 23;
-      uint32_t e : 8;
-      uint32_t signal : 1;
+	union float_ {
+  struct {
+      uint32_t man : 23;
+      uint32_t exp : 8;
+      uint32_t sign : 1;
     };
-    uint32_t value;
+    uint32_t val;
   };
   union float_ f;
-  f.value = *((uint32_t*)(void*)&a);
-
-  int e = f.e - 127;
-
-  FLOAT result;
-  if (e <= 7) {
-    result = (f.m | (1 << 23)) >> 7 - e;
+  f.val = *((uint32_t*)(void*)&a);
+  int exp = f.exp - 127;
+  FLOAT ret = 0;
+  if (exp == 128)
+    assert(0);
+  if (exp >= 0) {
+    int mov = 7 - exp;
+    if (mov >= 0)
+      ret = (f.man | (1 << 23)) >> mov;
+    else
+      ret = (f.man | (1 << 23)) << (-mov);
   }
-  else {
-    result = (f.m | (1 << 23)) << (e - 7);
-  }
-  return f.signal == 0 ? result : (result|(1<<31));
+  else
+    return 0;
+  return f.sign == 0 ? ret : -ret;
+}
+
+FLOAT Fabs(FLOAT a) {
+	if ((a & 0x80000000) == 0)
+		return a;
+	else
+		return -a;
 }
 
 /* Functions below are already implemented */
-FLOAT Fabs(FLOAT a)
-{
-  return (a > 0) ? a : -a;
-}
 
 FLOAT Fsqrt(FLOAT x) {
   FLOAT dt, t = int2F(2);
-  
+
   do {
     dt = F_div_int((F_div_F(x, t) - t), 2);
     t += dt;
   } while(Fabs(dt) > f2F(1e-4));
+
   return t;
 }
 
@@ -109,5 +95,6 @@ FLOAT Fpow(FLOAT x, FLOAT y) {
     dt = (F_div_F(x, t2) - t) / 3;
     t += dt;
   } while(Fabs(dt) > f2F(1e-4));
+
   return t;
 }

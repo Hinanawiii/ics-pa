ad036523dbfdc90810736fba91734755014410e4
tracer-ics2017 <tracer@njuics.org>
2025-06-09 02:41:04 +0800

    >  run 2212338 hinanawi Linux hinanawi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  02:41:04 up  8:13,  1 user,  load average: 0.67, 0.34, 0.20 7f875642febdb71975a18a01d0d5f35eb094010

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -75,7 +75,7 @@ ssize_t fs_read(int fd, void *buf, size_t len) {
 			break;
 		default:
 			//TODO
-			Log("buf=%p,disk_offset=%p,open_offset=%p,len=%d,fd=%d",buf,file_table[fd].disk_offset,file_table[fd].open_offset,len,fd);
+			//Log("buf=%p,disk_offset=%p,open_offset=%p,len=%d,fd=%d",buf,file_table[fd].disk_offset,file_table[fd].open_offset,len,fd);
 			ramdisk_read(buf,file_table[fd].disk_offset+file_table[fd].open_offset,len);
 			file_table[fd].open_offset +=len;
 			break;

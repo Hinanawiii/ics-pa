dfa3feabc021f58b803e05df00a49f698bb32e8e
tracer-ics2017 <tracer@njuics.org>
2025-06-09 03:48:00 +0800

    >  run 2212338 hinanawi Linux hinanawi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  03:48:00 up  9:20,  1 user,  load average: 0.26, 0.18, 0.34 62eb24c04dd045547ad1bb34b20e034d2c3a36da

--- backup/nanos-lite/src/fs.c
+++ backup/nanos-lite/src/fs.c
@@ -45,6 +45,7 @@ int fs_open(const char *pathname, int flags, int mode) {
 	for (i = 0; i < NR_FILES; i++) {
 	  //Log("filename:%s\n",file_table[i].name);
 		if (strcmp(file_table[i].name, pathname) == 0) {
+			file_table[i].open_offset=0;
 			 //Log("file opened,i=%d,NR_FILES=%d",i,NR_FILES);
 			return i;
 		}
--- nanos-lite/include/fs.h
+++ nanos-lite/include/fs.h
@@ -4,10 +4,10 @@
 #include "common.h"
 
 enum {SEEK_SET, SEEK_CUR, SEEK_END};
-int fs_open(const char *path,int,int);
-ssize_t fs_read(int,void*,size_t);
-ssize_t fs_write(int,void*,size_t);
-off_t fs_lseek(int,off_t,int);
+int fs_open(const char *pathname, int flags, int mode);
+ssize_t fs_read(int fd, void *buf, size_t len);
+ssize_t fs_write(int fd, const void *buf, size_t len);
+off_t fs_lseek(int fd, off_t offset, int whence);
 int fs_close(int fd);
-size_t fs_filesz(int fd);
+size_t fs_filesz(int fd) ;
 #endif
